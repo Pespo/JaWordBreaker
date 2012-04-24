46e8dd5bef8157a1705aef4b488f22d66404010b
Tom GAY <muxisar@gmail.com>
2012-04-24 21:43:54 +0200

    INIT

---
+++ src/fr/imac/jwbreaker/Main.java
@@ -0,0 +1,22 @@
+package fr.imac.jwbreaker;
+
+/**
+ * @author Tom
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+	}
+}
